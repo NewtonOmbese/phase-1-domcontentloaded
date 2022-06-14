29aa527ec97597c722d38e58bf00445bf9b64165
NewtonOmbese <newtonombese1@gmail.com>
2022-06-14 05:57:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    updatedDOM()
+});
+
+function updatedDOM(){
+    document.getElementById("text").textContent = "This is really cool!";
+}
